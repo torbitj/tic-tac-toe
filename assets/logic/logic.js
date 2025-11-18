e8ae140c4ef538730beca04ef22d1b3a0c1dae64
JT <jstorbit@gmail.com>
2025-11-18 09:23:34 -0700

    Add base component structure to board

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -4,14 +4,20 @@ const state = {
 
 const ARRAY_LENGTH = 3;
 
+const Board = () => {
+  const $board = document.createElement(`section`);
+  $board.innerHTML = `
+  <RowOne></RowOne>
+  <RowTwo></RowTwo>
+  <RowThree></RowThree>`;
+}
+
 const render = () => {
   const $app = document.querySelector(`#app`);
   $app.innerHTML = `
   <h1>Tic Tac Toe Game</h1>
   <Score></Score>
-  <section><RowOne></RowOne></section>
-  <section><RowTwo></RowTwo></section>
-  <section><RowThree></RowThree></section>
+  <Board></Board>
   `;
 }
 
