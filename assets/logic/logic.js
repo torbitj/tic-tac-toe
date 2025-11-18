911485e0ffb1fbb64f54bd32cc3d3a63f709bfe2
JT <jstorbit@gmail.com>
2025-11-18 08:42:01 -0700

    Add render function

---
+++ assets/logic/logic.js
@@ -0,0 +1,10 @@
+const render = () => {
+  const $app = document.querySelector(`#app`);
+  $app.innerHTML = `
+  <h1>Tic Tac Toe Game</h1>
+  <Score></Score>
+  <section><RowOne></RowOne></section>
+  <section><RowTwo></RowTwo></section>
+  <section><RowThree></RowThree></section>
+  `;
+}
